b8491a202678c370be366f782e630c794e55c748
tttabc123 <254030999@qq.com>
2026-09-13 21:34:44 +0800

    fix(core): preserve MCU integer widths in hosted tests

--- core/stc8h_types.h
+++ core/stc8h_types.h
@@ -1,6 +1,9 @@
 #ifndef STC8H_TYPES_H
 #define STC8H_TYPES_H
 
+/* Match the MCU wire/arithmetic widths when running C services on a host.
+ * SDCC and Keil keep their native 8051 types and require no stdint library. */
+#if defined(__SDCC) || defined(__C51__) || defined(__CX51__)
 typedef unsigned char stc8h_u8;
 typedef unsigned int stc8h_u16;
 typedef unsigned long stc8h_u32;
@@ -8,6 +11,16 @@ typedef unsigned long stc8h_u32;
 typedef signed char stc8h_s8;
 typedef signed int stc8h_s16;
 typedef signed long stc8h_s32;
+
+#else
+#include <stdint.h>
+typedef uint8_t stc8h_u8;
+typedef uint16_t stc8h_u16;
+typedef uint32_t stc8h_u32;
+typedef int8_t stc8h_s8;
+typedef int16_t stc8h_s16;
+typedef int32_t stc8h_s32;
+#endif
 
 typedef enum {
     STC8H_OK = 0,
